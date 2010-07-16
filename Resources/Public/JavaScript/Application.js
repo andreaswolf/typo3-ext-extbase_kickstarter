3a7a7d143588709bcbce208c9a0aa833818a1e97
Andreas Wolf <andreas.wolf@ikt-werk.de>
2010-07-16 17:40:46 +0200

    [BUGFIX] Interface did not render without Firebug enabled
    Added a mocked console object if console object is not available

--- Resources/Public/JavaScript/Application.js
+++ Resources/Public/JavaScript/Application.js
@@ -42,6 +42,14 @@ T3X.ExtbaseKickstarter.Application = Ext.apply(new Ext.util.Observable, {
 	 * This method is called automatically.
 	 */
 	bootstrap: function() {
+		if (!('console' in window) || !('firebug' in console)) {
+			var names = ['log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml', 'group', 'groupEnd', 'time', 'timeEnd', 'count', 'trace', 'profile', 'profileEnd'];
+			window.console = {};
+			for (var i = 0; i < names.length; ++i) {
+				window.console[names[i]] = function() {};
+			}
+		}
+
 		console.log('T3X.ExtbaseKickstarter.Application.bootstrap');
 
 		this._initializeConfiguration();
